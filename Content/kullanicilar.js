3c430b676eca60d19a4c99cc84f66f3fe6832d78
adnanarslangiray <adnan.arslangiray@gmail.com>
2018-01-29 02:29:02 +0300

    enson

--- Content/kullanicilar.js
+++ Content/kullanicilar.js
@@ -4,14 +4,14 @@
         
         "kullanici_adi":"Test",
         "sifre": "12345",
-        "hesapDurumu": "1"
+        "hesapDurumu": 1
         
     },
     {
 
         "kullanici_adi": "Adnan",
         "sifre": "12345",
-        "hesapDurumu": "1"
+        "hesapDurumu": 0
     }
   
 ]
